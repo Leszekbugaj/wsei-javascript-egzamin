24f511538a460854a4093d7d3a8625f167b3a317
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2019-12-06 15:23:58 +0100

    Update zadanie02.js

---
+++ js/zadanie02.js
@@ -0,0 +1,10 @@
+function concatArray(array1, array2) {
+    return array1.concat(array2);
+}
+
+var num1 = [13, 8];
+var num2 = [10, 30];
+
+console.log(num1);
+console.log(num2);
+console.log(concatArray(num1, num2));
