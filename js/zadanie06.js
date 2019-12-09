ca5fc31f7233f73f6dbf391914775e6c1d1c4f91
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2019-12-09 21:33:37 +0100

    Update zadanie06.js

---
+++ js/zadanie06.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var list = document.getElementById("shopping-list");
+
+    document.getElementById("button-1").addEventListener("kliknij", () => {
+        var newElement = document.createElement("LI");
+        
+        list.appendChild(newElement).innerHTML = "chleb";
+        
+    });
+
+    document.getElementById("button-2").addEventListener("kliknij", () => {
+        list.removeChild(list.children[list.children.length - 1]);
+        
+    });
+
+    document.getElementById("button-3").addEventListener("kliknij", () => {
+        var clone = list.children[1].cloneNode(true);
+        
+        list.appendChild(clone);
+        
+    });
+});
