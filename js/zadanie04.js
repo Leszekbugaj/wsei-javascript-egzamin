6c4e2a169f661c769a1095acb89a3df32e53944c
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2019-12-09 21:45:13 +0100

    Update zadanie04.js

--- js/zadanie04.js
+++ js/zadanie04.js
@@ -1,25 +1,28 @@
-//1. Szukanie nazw tagów:
-
+//1
 function getTagsOfElements(elements) {
+   
     var tags = [];
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i].tagName != null) tags.push(elements[i].tagName)
+ 
+    for (let n = 0; n < elements.length; n++) {
+        if (elements[n].tagName != null) tags.push(elements[n].tagName)
     }
+    
     return tags;
 }
 
 console.table(getTagsOfElements(document.getElementsByClassName("sample_class")));
 
-//2. Szukanie nazw klas:
-
+//2
 function getClassesOfElement(element) {
+    
     var className = element.className.split(' ');
     return className;
+    
 }
 
 console.table(getClassesOfElement(document.getElementById("sample_id")));
 
-//3. Szukanie tekstu:
+//3
 
 function getInnerTextsOfElements(elements) {
     var texts = elements[0].innerText.split('\n');;
@@ -27,26 +30,3 @@ function getInnerTextsOfElements(elements) {
 }
 console.table(getInnerTextsOfElements(document.getElementsByClassName("sample_class_2")));
 
-//4. Szukanie adresów linków:
-
-function getAddressesOfElements(elements) {
-    var links = []
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i].getAttribute("href") != null)
-            links.push(elements[i].getAttribute("href"));
-    }
-    return links;
-}
-
-console.table(getAddressesOfElements(document.getElementsByTagName("a")));
-
-//5. Szukanie tagów dzieci:
-var children = []
-var sample_class3 = document.getElementsByClassName("sample_class_3");
-for (const key in sample_class3) {
-    for (const child in sample_class3[key].children) {
-        children.push(sample_class3[key].children[child]);
-    }
-}
-
-console.table(getTagsOfElements(children));
